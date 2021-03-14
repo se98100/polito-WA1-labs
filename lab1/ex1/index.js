dff44d6b96b989fc0762b57d0dd2d0055bb8f617
se98100 <se98100@gmail.com>
2021-03-14 22:26:51 +0100

    filterAndPrint implemented

--- lab1/ex1/index.js
+++ lab1/ex1/index.js
@@ -14,24 +14,35 @@ function Task(id, desc, urgent = false, pri = true, deadline = null){
 
 function TaskList(){
     this.tasks = [];
+
     this.sortAndPrint = () => {
-        this.tasks.sort((a, b) => {
+        console.log('****** Tasks sorted by deadline (most recent first): ******');
+        this.tasks
+        .sort((a, b) => {
             if(a.deadline === null) return 1;
             if(b.deadline === null) return -1;
             return a.deadline.isBefore(b.deadline) ? -1 : 1
-        });
-        console.log('****** Tasks sorted by deadline (most recent first): ******');
-        this.tasks.forEach(task => console.log(`Id: ${task.id}, Description: ${task.description}, ` +
+        })
+        .forEach(task => console.log(`Id: ${task.id}, Description: ${task.description}, ` +
+            `Urgent: ${task.urgent}, Private: ${task.private}, Deadline: ${task.deadline != null ? task.deadline.format('LLL') : '<not defined>'}`));
+    }
+
+    this.filterAndPrint = () => {
+        console.log('****** Tasks filtered, only (urgent == true): ******');
+        this.tasks
+        .filter(task => task.urgent)
+        .forEach(task => console.log(`Id: ${task.id}, Description: ${task.description}, ` +
             `Urgent: ${task.urgent}, Private: ${task.private}, Deadline: ${task.deadline != null ? task.deadline.format('LLL') : '<not defined>'}`));
     }
 }
 
 var tl = new TaskList();
 tl.tasks.push(new Task(0, 'Prima', false, true, dayjs()));
-tl.tasks.push(new Task(1, 'Seconda', false, true, dayjs().add(5, 'd')));
+tl.tasks.push(new Task(1, 'Seconda', true, true, dayjs().add(5, 'd')));
 tl.tasks.push(new Task(2, 'Terza', false, true));
-tl.tasks.push(new Task(3, 'Quarta', false, true, dayjs().add(5, 'h')));
-tl.tasks.push(new Task(5, 'Sesta', false, true));
+tl.tasks.push(new Task(3, 'Quarta', true, true, dayjs().add(5, 'h')));
+tl.tasks.push(new Task(5, 'Sesta', true, true));
 tl.tasks.push(new Task(4, 'Quinta', false, true, dayjs().add(5, 'm')));
 
 tl.sortAndPrint();
+tl.filterAndPrint();
